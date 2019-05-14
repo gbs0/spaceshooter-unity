040d9ede2e5ae3cbc27fb0641f19178c2a7262ba
gabriel schiavo <gabriel@lewagon.org>
2019-05-14 20:20:57 -0300

    add fix for background controllers

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic; // For the Queue.
 public class BackgroundController : MonoBehaviour {
 
 	public GameObject[] BGObjects; // An array of BG prefabs.
+	public float yStore;
 
 	// Queue to hold the objects.
 	Queue<GameObject>availableObjects = new Queue<GameObject>();
@@ -13,14 +14,23 @@ public class BackgroundController : MonoBehaviour {
 	void Start () 
 	{
 		// Add the available objects to the queue.
-		availableObjects.Enqueue(BGObjects [0]); 
-		availableObjects.Enqueue(BGObjects [1]);
-		availableObjects.Enqueue(BGObjects [2]);
-		availableObjects.Enqueue(BGObjects [3]);
+		//availableObjects.Enqueue(BGObjects [0]); 
+		//availableObjects.Enqueue(BGObjects [1]);
+		//availableObjects.Enqueue(BGObjects [2]);
+		//availableObjects.Enqueue(BGObjects [3]);
 
 		// Call the MoveObjectDown function every 20 seconds.
-		InvokeRepeating("MoveObjectDown", 0, 20f);	
+		//InvokeRepeating("MoveObjectDown", 0, 1f);	
+		yStore = -5;
 	}
+
+
+	void Update(){
+
+		transform.Translate(new Vector2(0,yStore * Time.deltaTime));
+
+	}
+
 
 	// Function to dequeue an object, and set its isMoving flag to true
 	// so that the object will start to scroll down the screen.
@@ -48,6 +58,7 @@ public class BackgroundController : MonoBehaviour {
 			if((anObject.transform.position.y < 0) && (!anObject.GetComponent<BGObject>().isMoving))
 			{
 				// Reset the planet position.
+				yStore = -5;	
 				anObject.GetComponent<BGObject>().ResetPosition();
 				// Enqueue the object.
 				availableObjects.Enqueue(anObject);
